026d6d18bad27cee0ece47f53baa1d6de9aed7ce
adelquis-molinari <adelkis003@gmail.com>
2023-07-25 10:24:16 -0300

    added favoriteStar functionality

--- pruebas/01-reading-list/adelquis-molinari/lily/src/components/header/favoriteStar.tsx
+++ pruebas/01-reading-list/adelquis-molinari/lily/src/components/header/favoriteStar.tsx
@@ -1,9 +1,13 @@
+import { useFavoriteBooksStore } from "../../store/store";
 import { Star } from "../icons";
 export function FavoriteStar() {
+  const star = useFavoriteBooksStore((store) => store.favoriteBooksIds);
   return (
     <div className="flex relative">
       <Star width={24} height={24} />
-      <span className="absolute left-4 bottom-2 font-bold text-lg text-red-400"></span>
+      <span className="absolute left-4 bottom-2 font-bold text-lg text-red-400">
+        {star.length}
+      </span>
     </div>
   );
 }
