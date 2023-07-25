20ddc6077a18a9d15f66722298bd44911dd1ce6a
adelquis-molinari <adelkis003@gmail.com>
2023-07-25 09:31:52 -0300

    refactoring of types

--- pruebas/01-reading-list/adelquis-molinari/lily/type.ts
+++ pruebas/01-reading-list/adelquis-molinari/lily/type.ts
@@ -13,3 +13,20 @@ export interface AuthorBook {
   name: string;
   otherBooks: string[];
 }
+
+
+export interface PropsBook {
+  ISBN: string;
+  title: string;
+  cover: string;
+  isFavorite: boolean;
+}
+
+
+export interface favoriteBooksState {
+  storeBooks: Book[]
+  favoriteBooksIds: string[];
+  addFavoriteBook: (ISBN: string) => void;
+  removeFavoriteBook: (ISBN: string) => void;
+}
+
