cbcb0484d9397ed5dbd91001b65846addeca523a
adelquis-molinari <adelkis003@gmail.com>
2023-07-26 09:19:49 -0300

    change name side bar and add README

--- pruebas/01-reading-list/adelquis-molinari/lily/src/layout.tsx
+++ pruebas/01-reading-list/adelquis-molinari/lily/src/layout.tsx
@@ -1,15 +1,20 @@
 import { HeaderApp } from "./components/header/headerApp";
-import { SideBar } from "./components/sideBar";
-interface Props {
-  children: React.ReactNode;
-}
+import { SideBar } from "./components/sideBar/SideBar";
+import { FavoritesSection } from "./components/content/favoritesSection";
+import { Props } from "../type";
+import { useState } from "react";
 
 export function Layout(props: Props) {
+  const [favoriteSideBar, setFavoriteSideBar] = useState<boolean>(false);
+
+  const toggleFavoriteSideBar = () => setFavoriteSideBar(!favoriteSideBar);
+
   return (
     <>
-      <HeaderApp />
+      <HeaderApp toggleFavoriteSideBar={toggleFavoriteSideBar} />
       <SideBar />
       <main>{props.children}</main>
+      {favoriteSideBar && <FavoritesSection />}
     </>
   );
 }
